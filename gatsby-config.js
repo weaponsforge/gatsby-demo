24499fe0acc90d03a49592828ac0cf417e8adf17
weaponsforge <forgeweapons@gmail.com>
2021-08-23 02:22:15 +0800

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,19 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "gatsby-demo",
+    title: "My First Gatsby Site",
   },
   plugins: [
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
-    "gatsby-plugin-sharp"
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`
+      }
+    }
   ],
   pathPrefix: "/gatsby-demo"
 };
