2f4a7b9d4eb039768d26af60678754a933b1bf38
weaponsforge <forgeweapons@gmail.com>
2021-08-23 05:16:39 +0800

    Finished Gatsby Tutorial Part 5.

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "My First Gatsby Site",
   },
+  pathPrefix: "/gatsby-demo",
   plugins: [
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
@@ -13,7 +14,50 @@ module.exports = {
         name: `blog`,
         path: `${__dirname}/blog`
       }
-    }
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 650,
+            }
+          },
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: 'language-',
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: false,
+              noInlineHighlight: false,
+              languageExtensions: [
+                {
+                  language: "superscript",
+                  extend: "javascript",
+                  definition: {
+                    superscript_types: /(SuperType)/,
+                  },
+                  insertBefore: {
+                    function: {
+                      superscript_keywords: /(superif|superelse)/,
+                    },
+                  },
+                },
+              ],
+              prompt: {
+                user: "root",
+                host: "localhost",
+                global: false,
+              },
+              escapeEntities: {},
+            }
+          },
+          "gatsby-remark-autolink-headers",
+        ]
+      }
+    },
   ],
-  pathPrefix: "/gatsby-demo"
 };
